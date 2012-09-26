f52269f957dfc6e91a7f18a3d4ebda7adafa0941
teodoro <delacerna_teodoro@yahoo.com>
2012-09-26 12:31:05 -0700

    BP Collection Listing + filter barangay

--- services/bp-services.jar/META-INF/sql/bppayment.sql
+++ services/bp-services.jar/META-INF/sql/bppayment.sql
@@ -19,14 +19,23 @@ SELECT
 	SUM(CASE WHEN ia.systype IS NULL OR ia.systype NOT IN( 'BUSINESS_TAX', 'REG_FEE') THEN bpp.amount + bpp.surcharge + bpp.interest - bpp.discount ELSE 0.0 END) AS otherfee, 
 	sum( bpp.total ) as total 
 FROM bppayment bpp 
-INNER JOIN business b ON b.applicationid = bpp.applicationid 
+INNER JOIN bpapplicationlisting b ON b.objid = bpp.applicationid 
 INNER JOIN incomeaccount ia ON ia.objid = bpp.acctid  
 INNER JOIN entity e ON e.objid = b.taxpayerid 
 WHERE bpp.iyear = $P{iyear} 
  AND bpp.iqtr = $P{iqtr} 
  AND bpp.imonth LIKE $P{imonth} 
  AND bpp.iday LIKE $P{iday} 
+ AND b.barangayid LIKE $P{barangayid}
  AND voided = 0 
 GROUP BY b.tradename, b.businessaddress, b.taxpayername, e.entityaddress  
+
+
+[getBarangayList]
+SELECT objid AS barangayid, lguname AS barangayname FROM lgu WHERE lgutype = 'BARANGAY'  ORDER BY lguname  
+
+
+
+
 
 
