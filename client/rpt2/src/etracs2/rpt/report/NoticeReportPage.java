0228472183dfb5edc61cecb5a62e5c71293ef782
Mary Grace Cabantan <cabantan_mg@yahoo.com>
2012-11-20 16:12:45 +0800

    Notice Of Assessment Report   : Change the data structure 			        Add Receive button 			        Use two report templates
    Notice Of Assessment 2 Report : Use only one report template

--- client/rpt2/src/etracs2/rpt/report/NoticeReportPage.java
+++ client/rpt2/src/etracs2/rpt/report/NoticeReportPage.java
@@ -6,10 +6,13 @@
 
 package etracs2.rpt.report;
 
+import com.rameses.rcp.ui.annotations.StyleSheet;
+
 /**
  *
  * @author  jzamora
  */
+@StyleSheet("etracs2/rpt/report/NoticeReportPage.style")
 public class NoticeReportPage extends javax.swing.JPanel {
     
     /** Creates new form NoticeReportPage */
@@ -24,74 +27,109 @@ public class NoticeReportPage extends javax.swing.JPanel {
      */
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
+        xActionBar1 = new com.rameses.rcp.control.XActionBar();
+        jPanel1 = new javax.swing.JPanel();
         formPanel1 = new com.rameses.rcp.util.FormPanel();
-        xLookupField1 = new com.rameses.rcp.control.XLookupField();
+        formPanel4 = new com.rameses.rcp.util.FormPanel();
+        xRadio3 = new com.rameses.rcp.control.XRadio();
+        xLookupField4 = new com.rameses.rcp.control.XLookupField();
+        formPanel5 = new com.rameses.rcp.util.FormPanel();
+        xRadio4 = new com.rameses.rcp.control.XRadio();
         xLookupField2 = new com.rameses.rcp.control.XLookupField();
-        xButton1 = new com.rameses.rcp.control.XButton();
-        xButton2 = new com.rameses.rcp.control.XButton();
+
+        setLayout(new java.awt.BorderLayout());
+
+        xActionBar1.setBorder(new com.rameses.rcp.control.border.XUnderlineBorder());
+        xActionBar1.setName("formActions");
+        add(xActionBar1, java.awt.BorderLayout.NORTH);
 
         com.rameses.rcp.control.border.XTitledBorder xTitledBorder1 = new com.rameses.rcp.control.border.XTitledBorder();
         xTitledBorder1.setTitle("Notice of Assessment Initial Information");
         formPanel1.setBorder(xTitledBorder1);
-        xLookupField1.setCaption("Taxpayer");
-        xLookupField1.setExpression("#{entityname}");
-        xLookupField1.setHandler("lookupTaxpayer");
-        xLookupField1.setName("taxpayer");
-        xLookupField1.setPreferredSize(new java.awt.Dimension(0, 19));
-        xLookupField1.setRequired(true);
-        formPanel1.add(xLookupField1);
+
+        formPanel4.setCellpadding(new java.awt.Insets(0, 0, 0, 20));
+        formPanel4.setOrientation(com.rameses.rcp.constant.UIConstants.HORIZONTAL);
+        formPanel4.setShowCaption(false);
+        xRadio3.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        xRadio3.setSelected(true);
+        xRadio3.setText("By Taxpayer :");
+        xRadio3.setCaption("By Taxpayer");
+        xRadio3.setMargin(new java.awt.Insets(0, 0, 0, 0));
+        xRadio3.setName("searchcriteria");
+        xRadio3.setOpaque(false);
+        xRadio3.setOptionValue("bytaxpayer");
+        xRadio3.setShowCaption(false);
+        formPanel4.add(xRadio3);
+
+        xLookupField4.setCaption("Taxpayer");
+        xLookupField4.setDepends(new String[] {"searchcriteria"});
+        xLookupField4.setExpression("#{entityname}");
+        xLookupField4.setHandler("lookupTaxpayer");
+        xLookupField4.setName("taxpayer");
+        xLookupField4.setPreferredSize(new java.awt.Dimension(250, 19));
+        xLookupField4.setShowCaption(false);
+        formPanel4.add(xLookupField4);
+
+        formPanel1.add(formPanel4);
+
+        formPanel5.setCellpadding(new java.awt.Insets(0, 0, 0, 20));
+        formPanel5.setOrientation(com.rameses.rcp.constant.UIConstants.HORIZONTAL);
+        formPanel5.setPadding(new java.awt.Insets(0, 5, 5, 5));
+        formPanel5.setShowCaption(false);
+        xRadio4.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        xRadio4.setText("By TD No. :");
+        xRadio4.setCaption("By Taxpayer");
+        xRadio4.setMargin(new java.awt.Insets(0, 0, 0, 0));
+        xRadio4.setName("searchcriteria");
+        xRadio4.setOpaque(false);
+        xRadio4.setOptionValue("bytdno");
+        xRadio4.setPreferredSize(new java.awt.Dimension(85, 15));
+        xRadio4.setShowCaption(false);
+        formPanel5.add(xRadio4);
 
         xLookupField2.setCaption("TD No.");
+        xLookupField2.setDepends(new String[] {"searchcriteria"});
         xLookupField2.setDynamic(true);
         xLookupField2.setExpression("#{tdno}");
         xLookupField2.setHandler("lookupFAAS");
         xLookupField2.setName("faas");
-        xLookupField2.setPreferredSize(new java.awt.Dimension(200, 19));
-        xLookupField2.setRequired(true);
-        formPanel1.add(xLookupField2);
+        xLookupField2.setPreferredSize(new java.awt.Dimension(250, 19));
+        xLookupField2.setShowCaption(false);
+        formPanel5.add(xLookupField2);
 
-        xButton1.setMnemonic('v');
-        xButton1.setText("Preview");
-        xButton1.setName("preview");
+        formPanel1.add(formPanel5);
 
-        xButton2.setMnemonic('p');
-        xButton2.setText("Print");
-        xButton2.setName("print");
-
-        org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(layout.createSequentialGroup()
+        org.jdesktop.layout.GroupLayout jPanel1Layout = new org.jdesktop.layout.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
-                    .add(layout.createSequentialGroup()
-                        .add(xButton2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(xButton1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                    .add(formPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 471, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(21, Short.MAX_VALUE))
+                .add(formPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 413, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(40, Short.MAX_VALUE))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(layout.createSequentialGroup()
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .add(formPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 199, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(xButton1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(xButton2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(61, Short.MAX_VALUE))
+                .add(formPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 145, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(32, Short.MAX_VALUE))
         );
+        add(jPanel1, java.awt.BorderLayout.CENTER);
+
     }// </editor-fold>//GEN-END:initComponents
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.rameses.rcp.util.FormPanel formPanel1;
-    private com.rameses.rcp.control.XButton xButton1;
-    private com.rameses.rcp.control.XButton xButton2;
-    private com.rameses.rcp.control.XLookupField xLookupField1;
+    private com.rameses.rcp.util.FormPanel formPanel4;
+    private com.rameses.rcp.util.FormPanel formPanel5;
+    private javax.swing.JPanel jPanel1;
+    private com.rameses.rcp.control.XActionBar xActionBar1;
     private com.rameses.rcp.control.XLookupField xLookupField2;
+    private com.rameses.rcp.control.XLookupField xLookupField4;
+    private com.rameses.rcp.control.XRadio xRadio3;
+    private com.rameses.rcp.control.XRadio xRadio4;
     // End of variables declaration//GEN-END:variables
     
 }
