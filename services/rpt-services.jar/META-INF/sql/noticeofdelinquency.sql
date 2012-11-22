eac0b3f3de2f5b64232d4e073f3acc69ab4c7569
teodoro <delacerna_teodoro@yahoo.com>
2012-11-21 16:11:51 -0800

    Demand Letters  - temporary for checking

---
+++ services/rpt-services.jar/META-INF/sql/noticeofdelinquency.sql
@@ -0,0 +1,28 @@
+
+[getList]
+SELECT 
+ * 
+FROM noticeofdelinquency  
+WHERE doctype LIKE $P{doctype} 
+
+
+[getListByDoctype]
+SELECT 
+ * 
+FROM noticeofdelinquency  
+WHERE doctype LIKE $P{doctype} 
+
+
+[getLedgerForNoticeOfDeliquency] 
+SELECT   
+	objid, taxpayerid, taxpayerno, taxpayername,   
+	taxpayeraddress, administratorname, administratoraddress   
+FROM rptledger   
+WHERE lastyearpaid <= $P{value}    
+AND docstate = 'APPROVED' 
+
+[updateNODByType]  
+UPDATE noticeofdelinquency   
+SET doctype = $P{doctype}  
+WHERE objid = $P{objid}  
+ 
